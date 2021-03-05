ddcd6d03c434eb4d05d3973d7716e8c482369320
Gasper <rataj.gasper@gmail.com>
2021-03-05 20:51:35 +0100

    Second commit

---
+++ src/components/Button.js
@@ -0,0 +1,10 @@
+function Button({ color, text, onClick }) {
+
+    return (
+        <button className="btn" style={{ backgroundColor: color }} onClick={onClick}>
+            {text}
+        </button>
+    )
+}
+
+export default Button
